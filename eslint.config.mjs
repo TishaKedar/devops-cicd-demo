d1b3e31d03ccff284c7c9945eaf53d5dea50ee0e
Tisha Kedar <23amtics147@gmail.com>
2026-07-10 22:28:54 +0530

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,14 +10,29 @@ export default defineConfig([
     languageOptions: {
       globals: {
         ...globals.node,
-        ...globals.browser
-      }
-    }
+        ...globals.browser,
+      },
+    },
   },
+
+  // CommonJS for Node files
   {
     files: ["**/*.js"],
     languageOptions: {
-      sourceType: "commonjs"
-    }
-  }
+      sourceType: "commonjs",
+    },
+  },
+
+  // Jest test files
+  {
+    files: ["tests/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        describe: "readonly",
+        test: "readonly",
+        expect: "readonly",
+      },
+    },
+  },
 ]);
